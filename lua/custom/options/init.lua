8cb0443092779ab25fd8ac3fb33e8ba5447e89db
Cheng Wang <galaxea1111@gmail.com>
2023-03-03 19:39:12 -0800

    _

--- lua/custom/options/init.lua
+++ lua/custom/options/init.lua
@@ -4,6 +4,8 @@ vim.o.autoindent = true
 vim.keymap.set({"n", "i"}, "<leader>t", ":NvimTreeToggle<CR>", { desc = '[T]ree toggle' })
 vim.keymap.set({"n", "v"}, "<c-j>", "5j")
 vim.keymap.set({"n", "v"}, "<c-k>", "5k")
+vim.keymap.set({"n", "v"}, "<c-h>", "b")
+vim.keymap.set({"n", "v"}, "<c-l>", "e")
 vim.keymap.set({"n", "v"}, "H", "^")
 vim.keymap.set({"n", "v"}, "L", "$")
 vim.keymap.set("n", "<c-s>", ":w<cr>")
