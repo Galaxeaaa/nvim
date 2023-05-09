f13c233b4bebcb12e27526d2fb81165c08823ffe
Cheng Wang <galaxea1111@gmail.com>
2023-05-09 10:18:49 -0700

    Update keybinding.

--- lua/custom/options/init.lua
+++ lua/custom/options/init.lua
@@ -1,7 +1,7 @@
 vim.o.shiftwidth = 4
 vim.o.autoindent = true
 
-vim.keymap.set({"n", "i"}, "<leader>t", ":NvimTreeToggle<CR>", { desc = '[T]ree toggle' })
+vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>", { desc = '[T]ree toggle' })
 vim.keymap.set({"n", "v"}, "<c-j>", "5j")
 vim.keymap.set({"n", "v"}, "<c-k>", "5k")
 vim.keymap.set({"n", "v"}, "<c-h>", "b")
@@ -10,4 +10,8 @@ vim.keymap.set({"n", "v"}, "H", "^")
 vim.keymap.set({"n", "v"}, "L", "$")
 vim.keymap.set("n", "<c-s>", ":w<cr>")
 vim.keymap.set("n", "<cr>", "o")
+vim.keymap.set("n", "<a-h>", "<c-w>h")
+vim.keymap.set("n", "<a-j>", "<c-w>j")
+vim.keymap.set("n", "<a-k>", "<c-w>k")
+vim.keymap.set("n", "<a-l>", "<c-w>l")
 
